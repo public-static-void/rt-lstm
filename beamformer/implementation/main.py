fbe19d4814958d6c3a33e2b8e102a7e327fdca88
public-static-void <ruthless_one@hotmail.de>
2022-05-12 16:45:07 +0200

    implemented room model and stft

--- beamformer/implementation/main.py
+++ beamformer/implementation/main.py
@@ -13,6 +13,9 @@ Topic         : Beamformer: Filter-And-Sum Implementation
 import numpy as np
 import pyroomacoustics as pra
 from scipy.io import wavfile
+from scipy.signal import get_window
+import matplotlib.pyplot as plt
+import soundfile as sf
 
 def create_room(rt60:float,room_dim:np.ndarray,fs:int) -> pra.room:
     """Helper function. Creates a room from input parameters and returns it.
@@ -32,9 +35,16 @@ def create_room(rt60:float,room_dim:np.ndarray,fs:int) -> pra.room:
     )
     return room
 
-def add_sources(room:pra.room, source_locs:np.ndarray, audio_path:str, delay:float):
+def add_sources(room:pra.room, source_locs:np.ndarray, audio:np.ndarray, delay:float):
+    """Helper function. Adds sources to an input room defined by input paramters.
 
-    _, audio = wavfile.read(audio_path)
+    :param room: pra.room
+    :param source_locs: np.ndarray
+    :param audio: np.ndarray
+    :param delay: float
+    :return: None
+    """
+
     # place the source in the room
     room.add_source(source_locs, signal=audio, delay=delay)
 
@@ -51,6 +61,66 @@ def add_mics(room:pra.room,mic_locs:np.ndarray):
 
 
 
+def plot_signal(s: np.ndarray, t: np.ndarray, title: str):
+    """Helper function. Plots a signal as magnitude `s` against time `t`.
+
+    Parameters
+    ----------
+    s : np.ndarray
+        Amplitude values of the input signal.
+    t : np.ndarray
+        Time values of the input signal.
+    title : str
+        Title for the plot.
+    """
+
+    # Define some general settings for the plot.
+    rows = 1
+    cols = 1
+    fig = plt.figure(figsize=(19, 7))
+
+    plt.suptitle(title)
+
+    plt.subplots_adjust(left=0.05,
+                        # bottom=0.1,
+                        right=0.99,
+                        # top=0.9,
+                        # wspace=0.4,
+                        hspace=0.4)
+
+    # First subplot.
+    ax = fig.add_subplot(rows, cols, 1)
+    ax.plot(t, s, 'b', label="s(t)")
+    ax.set_xlabel('t[s]')
+    ax.set_ylabel('s(t)')
+    ax.grid(True)
+    ax.legend()
+
+
+def plot_dft(S: np.ndarray, title: str):
+    """Helper function. Plots amplitude of `S` in dB together with the
+    amplitude of the corresponding filter `H`(z) in dB in one plot.
+
+    Parameters
+    ----------
+    S : np.ndarray
+        Amplitudes of input signal.
+    H : np.ndarray
+        Amplitudes of the corresponding filter.
+    title : str
+        Plot title.
+    """
+
+    fig, ax1 = plt.subplots()
+
+    ax1.set_title(title)
+    ax1.plot(S)
+
+    ax1.set_ylabel('Amplitude [dB]', color='b')
+
+    ax1.grid()
+    ax1.axis('tight')
+    ax1.legend()
 
 
 
@@ -72,7 +142,47 @@ def main():
     source_locs = [2.5, 3.73, 1.76]
 
     # Define sample path.
-    sample_path = "../../Samples/2213f081fd811e304423f283850b-orig.wav"
+    sample_path1 = "./Samples/2213f081fd811e304423f283850b-orig.wav"
+    sample_path2 = "./Samples/examples_input_samples_german_speech_8000.wav"
+
+
+
+    #fs1, audio1 = wavfile.read(sample_path1)
+    #fs2, audio2 = wavfile.read(sample_path2)
+
+    audio1,fs1 = sf.read(sample_path1)
+    audio2,fs2 = sf.read(sample_path2)
+
+
+    #print(audio1.dtype)
+
+    # Create time domain values.
+    t1 = np.arange(0, len(audio1))/fs1
+    t2 = np.arange(0, len(audio2))/fs2
+
+
+
+    # Frame length in ms.
+    fl = 32
+
+    # Transform frame length from ms to samples.
+    N1 = int(fs1*fl/1000)
+    N2 = int(fs2*fl/1000)
+
+
+    hop1 = int(N1/2)
+    hop2 = int(N2/2)
+
+    window1 = np.sqrt(get_window('hann', N1, fftbins=True))
+    window2 = np.sqrt(get_window('hann', N2, fftbins=True))
+
+    # Specify number of channels.
+    n_chans1 = 2
+    n_chans2 = 1
+
+    mySTFT1 = pra.transform.stft.STFT(N=N1,hop=hop1,analysis_window=window1,synthesis_window=window1,channels=n_chans1)
+    mySTFT2 = pra.transform.stft.STFT(N=N2, hop=hop2, analysis_window=window2, synthesis_window=window2,channels=n_chans2)
+
     # Specify delay.
     delay = 1.3
 
@@ -81,8 +191,31 @@ def main():
     # Add mics.
     add_mics(room1,mic_locs)
     # Add source(s).
-    add_sources(room1,source_locs,sample_path, delay)
+    add_sources(room1,source_locs,audio2, delay)
 
+
+    dft1 = mySTFT1.analysis(audio1)
+    idft1 = mySTFT1.synthesis(dft1)
+    t_idft_1 = np.arange(0, len(idft1))/fs1
+
+    plot_signal(audio1,t1,"audio1")
+    plot_signal(idft1, t_idft_1, "audio1 synthesis")
+
+
+    dft2 = mySTFT2.analysis(audio2)
+    idft2 = mySTFT2.synthesis(dft2)
+    t_idft_2 = np.arange(0, len(idft2))/fs2
+
+    plot_signal(audio2,t2,"audio2")
+    plot_signal(idft2, t_idft_2, "audio2 synthesis")
+
+    #print(idft1.dtype)
+
+    sf.write("audio1.wav",idft1,fs1)
+    sf.write("audio2.wav",idft2, fs2)
+
+
+    plt.show()
 
 
 if __name__ == "__main__":
