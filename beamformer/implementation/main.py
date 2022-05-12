6058ec95d6f5ce9ccc3ded718685d4269b4d05ae
public-static-void <ruthless_one@hotmail.de>
2022-05-12 14:28:16 +0200

    init project

---
+++ beamformer/implementation/main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Authors       : Vadim Titov, Henning Möllers, Leon Mannweiler
+Matr.-Nr.     : 6021356, ..., ...
+Created       : May 12th, 2022
+Last modified : May 12th, 2022
+Description   : Master's Project "Source Separation for Robot Control"
+Topic         : Beamformer: Filter-And-Sum Implementation
+"""
+
+import pyroomacoustics
+
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
