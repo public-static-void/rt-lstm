8f76b779d5ccfa49b47ee4f4ad77a49328ef51b6
public-static-void <ruthless_one@hotmail.de>
2022-07-07 14:43:19 +0200

    added tensorboard logging

--- dnn/implementation/net.py
+++ dnn/implementation/net.py
@@ -5,7 +5,7 @@
 Authors       : Vadim Titov
 Matr.-Nr.     : 6021356
 Created       : June 23th, 2022
-Last modified : June 23th, 2022
+Last modified : July 7th, 2022
 Description   : Master's Project "Source Separation for Robot Control"
 Topic         : Net module of the LSTM RNN Project
 """
@@ -13,24 +13,31 @@ Topic         : Net module of the LSTM RNN Project
 import pytorch_lightning as pl
 import torch.nn.functional as F
 from pytorch_lightning.callbacks import EarlyStopping
-
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning import loggers as pl_loggers
 
 # Hyper-parameters
 input_size = 512  # TODO: specify input size.
 hidden_size_1 = 256
 hidden_size_2 = 128
-output_size = 2  # 2 channels: 1 Re 1 Im. TODO: how to ensure each part ends up wherre it belongs?
+output_size = 2  # 2 channels: 1 Re 1 Im ?.
+# TODO: how to ensure each part ends up where it belongs?
 batch_size = 512
-num_epochs = 5  # TODO: specify number of epochs.
+num_epochs = 10  # TODO: specify number of epochs.
 learning_rate = 0.001
 num_workers = 4
 num_devices = 1
 device = 'gpu'
 is_test_run = False
-# Callbacks/Checkpoints.
-early_stopping = EarlyStopping(monitor='val_loss', patience=2, mode='min')  # Early stopping callback.
-checkpoint_callback = ModelCheckpoint(save_top_k=2, monitor='val_loss')
 
+# Callbacks/Checkpoints.
+early_stopping = EarlyStopping(monitor='val/loss', patience=5, mode='min')
+checkpointing = ModelCheckpoint(dirpath=None, filename='{epoch}-{step}',
+                                save_top_k=2, monitor='val/loss', every_n_epochs=1)
+
+# LOGGING
+LOG_DIR = 'logs/'
+tb_logger = pl_loggers.TensorBoardLogger(LOG_DIR, name='DeepClustering', log_graph=False)
 
 class LitNeuralNet(pl.LightningModule):
     def __init__(self, input_size, hidden_size_1, hidden_size_2, output_size):
@@ -62,7 +69,9 @@ class LitNeuralNet(pl.LightningModule):
         # MSE loss function.
         loss = F.mse_loss(outputs, labels)
 
-        tensorboard_logs = {'train_loss': loss}
+        tensorboard_logs = {'train/loss': loss}
+        self.log(f'train/loss', loss, on_step=False,
+                 on_epoch=True, logger=True)
 
         return {'loss': loss, 'log': tensorboard_logs}
 
@@ -76,7 +85,6 @@ class LitNeuralNet(pl.LightningModule):
                                                    batch_size=batch_size,
                                                    num_workers=num_workers,
                                                    shuffle=True)
-
         return train_loader
 
     def validation_step(self, batch, batch_idx):
@@ -87,7 +95,9 @@ class LitNeuralNet(pl.LightningModule):
         outputs = self(input)
         # MSE loss function.
         loss = F.mse_loss(outputs, labels)
-        return {'val_loss': loss}
+        self.log(f'val/loss', loss, on_step=False, on_epoch=True, logger=True)
+
+        return {'val/loss': loss}
 
     # TODO: Get training data from outside this module.
     def val_dataloader(self, val_dataset):
@@ -95,13 +105,13 @@ class LitNeuralNet(pl.LightningModule):
                                                  batch_size=batch_size,
                                                  num_workers=num_workers,
                                                  shuffle=False)
-
         return val_loader
 
     def validation_epoch_end(self, outputs):
-        avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
-        tensorboard_logs = {'avg_val_loss': avg_loss}
-        return {'val_loss': avg_loss, 'log': tensorboard_logs}
+        avg_loss = torch.stack([x['val/loss'] for x in outputs]).mean()
+        tensorboard_logs = {'avg_val/loss': avg_loss}
+
+        return {'val/loss': avg_loss, 'log': tensorboard_logs}
 
 
 def main():
@@ -113,11 +123,12 @@ def main():
     trainer = pl.Trainer(fast_dev_run=is_test_run, accelerator=device,
                          devices=num_devices, max_epochs=num_epochs,
                          enable_checkpointing=True, callbacks=[early_stopping,
-                                                               checkpoint_callback])
+                                                               checkpointing],
+                         log_every_n_steps=1, logger=tb_logger)
     model = LitNeuralNet(input_size, hidden_size, num_classes)
     print(model)
     trainer.fit(model)
-    checkpoint_callback.best_model_path
+    checkpointing.best_model_path
 
 if __name__ == '__main__':
     main()
