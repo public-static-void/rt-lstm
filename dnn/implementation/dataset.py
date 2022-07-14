d7de76a8bb06ab7e7b3714378dbcd0628532d978
Leon Mannweiler <xmannwei@informatik.uni-hamburg.de>
2022-07-14 15:41:57 +0200

    changes to Dataset

--- dnn/implementation/dataset.py
+++ dnn/implementation/dataset.py
@@ -12,13 +12,13 @@ class CustomDataset(Dataset):
     def __init__(self, type):
         self.type = type
 
-        if self.type == 'training':
-            self.data_dir = '/export/scratch/soundfiles/Training'
+        if self.type == 'Training':
+            self.data_dir = './dnn/imlementation/soundfiles/Training'
         else:
-            if self.type == 'validation':
-                self.data_dir = '/export/scratch/soundfiles/Validation'
+            if self.type == 'Validation':
+                self.data_dir = './dnn/implementation/soundfiles/Validation'
             else:
-                self.data_dir = '/export/scratch/soundfiles/Test'
+                self.data_dir = './dnn/implementation/soundfiles/Test'
 
         self.data_clean = np.sort(np.array(glob.glob(self.data_dir+"/*clean.wav")))
         self.data_noise = np.sort(np.array(glob.glob(self.data_dir+"/*noise.wav")))
@@ -28,6 +28,7 @@ class CustomDataset(Dataset):
         #TODO: *3?
     def __len__(self):
         return self.data_clean.shape[0]
+
 
     
     #TODO: Kontrolliere Fensterbreite... 256?
@@ -43,24 +44,25 @@ class CustomDataset(Dataset):
         noise_freqs, noise_times, noise_power = stft(noise_read[0], noise_read[1], window=window1, nperseg=512)
         mixture_freqs, mixture_times, mixture_power = stft(mixture_read[0], mixture_read[1], window=window1, nperseg=512)
 
-        #print(clean_power.shape)
-        #print(noise_power.shape)
-        #print(mixture_power.shape)
+        print(clean_power.shape)
+        print(noise_power.shape)
+        print(mixture_power.shape)
 
 
         clean_split_concatenate = np.concatenate((np.real(clean_power), np.imag(clean_power)), axis=0)
         noise_split_concatenate = np.concatenate((np.real(noise_power), np.imag(noise_power)), axis=0)
         mixture_split_concatenate = np.concatenate((np.real(mixture_power), np.imag(mixture_power)), axis=0)
 
-        #print(clean_split_concatenate)
-        #print(noise_split_concatenate)
-        #print(mixture_split_concatenate)
+
+        print(clean_split_concatenate.shape)
+        print(noise_split_concatenate.shape)
+        print(mixture_split_concatenate.shape)
 
         return clean_split_concatenate, noise_split_concatenate, mixture_split_concatenate
 
 
 
-#Dataset = CustomDataset('test')
+Dataset = CustomDataset('Training')
 
-#Dataset.__getitem__(0)
+Dataset.__getitem__(0)
 
