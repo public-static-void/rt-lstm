618cdea8b514b5696673414fdf7b4d34038c56af
Leon Mannweiler <xmannwei@informatik.uni-hamburg.de>
2022-11-27 18:14:45 +0100

    Added function for pesq.

--- dnn/implementation/performance.py
+++ dnn/implementation/performance.py
@@ -5,31 +5,32 @@ import glob
 from torchmetrics.audio.pesq import PerceptualEvaluationSpeechQuality
 import hyperparameters
 
-pesq = PerceptualEvaluationSpeechQuality(16000, 'wb')
+def __calculate_pesq__(self):
+    pesq = PerceptualEvaluationSpeechQuality(16000, 'wb')
 
-#TODO: 2 pesq's berechnen. 1.: clean und prediction 2.: noise und prediction. Dann ein Delta berechnen: 1.-2. Im Predict würde das dann aufgerufen werden.
+    #TODO: 2 pesq's berechnen. 1.: clean und prediction 2.: noise und prediction. Dann ein Delta berechnen: 1.-2. Im Predict würde das dann aufgerufen werden.
 
-data_clean = glob.glob(hyperparameters.OUT_DIR+"/clean*.wav")
+    data_clean = glob.glob(hyperparameters.OUT_DIR+"/clean*.wav")
 
-#print(data_clean)
+    #print(data_clean)
 
-dictionary = {}
-for file_name in data_clean:
-    clean_file, _ = torchaudio.load(file_name)
-    mix_file, _ = torchaudio.load(file_name.replace('clean', 'mix'))
-    #mix_file, _ = torchaudio.load(file_name.replace('clean', 'mixture'))
-    pred_file, _ = torchaudio.load(file_name.replace('clean', 'pred'))
-    #pred_file, _ = torchaudio.load(file_name.replace('clean', 'mixture'))
-    noise_file = mix_file - clean_file
+    dictionary = {}
+    for file_name in data_clean:
+        clean_file, _ = torchaudio.load(file_name)
+        mix_file, _ = torchaudio.load(file_name.replace('clean', 'mix'))
+        #mix_file, _ = torchaudio.load(file_name.replace('clean', 'mixture'))
+        pred_file, _ = torchaudio.load(file_name.replace('clean', 'pred'))
+        #pred_file, _ = torchaudio.load(file_name.replace('clean', 'mixture'))
+        noise_file = mix_file - clean_file
 
-    pesq_pred_to_clean = pesq(pred_file[0], clean_file[0])
-    pesq_pred_to_noise = pesq(pred_file[0], noise_file[0])
-    pesq_delta = pesq_pred_to_clean - pesq_pred_to_noise
+        pesq_pred_to_clean = pesq(pred_file[0], clean_file[0])
+        pesq_pred_to_noise = pesq(pred_file[0], noise_file[0])
+        pesq_delta = pesq_pred_to_clean - pesq_pred_to_noise
 
-    #Abspeichern als dictionary inkl. Dateinamen
+        #Abspeichern als dictionary inkl. Dateinamen
 
-    dictionary[file_name] = pesq_delta.item()
+        dictionary[file_name] = pesq_delta.item()
 
-print(dictionary)
+    print(dictionary)
 
 
